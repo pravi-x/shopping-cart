d0a3ebb139abaf9d9fbddac7a5ff68b7e9530f09
pravi-x <1998.xar@gmail.com>
2025-05-26 19:36:46 +0300

    add carousel in Product List

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import NavigationBar from "./components/NavigationBar.jsx";
 import HomePage from "./components/HomePage.jsx";
 import products from "./dummyData.js";
 import ProductList from "./components/ProductList.jsx";
+// πχ στο App.jsx
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   const [data, setData] = useState([]);
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,30 @@
+import Slider from "react-slick";
 import Product from "./Product";
 import styles from "./ProductList.module.css";
 
 function ProductList({ data }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    adaptiveHeight: true,
+  };
+  console.log("Products data:", data);
+
   return (
-    <div className={styles.productList}>
-      {data.map((p) => (
-        <Product name={p.name} img={p.img} price={p.price} />
-      ))}
-    </div>
+    <>
+      <h1 className={styles.centerTitle}>Products</h1>
+      <div style={{ padding: "1rem" }}>
+        <Slider {...settings}>
+          {data.map((p, i) => (
+            <Product key={i} name={p.name} img={p.img} price={p.price} />
+          ))}
+        </Slider>
+      </div>
+    </>
   );
 }
 
