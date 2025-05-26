c26f81241460c2b5863cadc6b3c09e82636434a2
pravi-x <1998.xar@gmail.com>
2025-05-26 19:16:24 +0300

    update navbar style

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,12 +1,29 @@
 import styles from "./NavigationBar.module.css";
+import { useState, useEffect } from "react";
 
 function NavigationBar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div className={styles.navContainer}>
-      <p className={styles.navLink}>Home</p>
-      <p className={styles.navLink}>Products</p>
-      <p className={styles.navLink}>My Cart</p>
-    </div>
+    <nav
+      className={`${styles.navContainer} ${isScrolled ? styles.scrolled : ""}`}
+    >
+      <div className={styles.leftLinks}>
+        <p className={styles.navLink}>Home</p>
+        <p className={styles.navLink}>Products</p>
+      </div>
+      <div className={styles.rightLinks}>
+        <p className={styles.navLink}>My Cart</p>
+      </div>
+    </nav>
   );
 }
 
