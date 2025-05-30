1567faf807f4619f29a02fb96833284b2d68d29e
pravi-x <1998.xar@gmail.com>
2025-05-30 18:05:32 +0300

    remove vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,47 @@
 import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+
 import NavigationBar from "./components/NavigationBar.jsx";
 import Footer from "./components/Footer.jsx";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Outlet } from "react-router-dom";
 import { ProductContext } from "./ProductContext.jsx";
 
-import { Analytics } from "@vercel/analytics/next";
+// Styles
+import "react-toastify/dist/ReactToastify.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/")
-      .then((res) => res.json())
-      .then((products) => {
-        // Add countInCart: 0 to each product
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products/");
+        const products = await res.json();
+
         const productsWithCount = products.map((product) => ({
           ...product,
           countInCart: 0,
         }));
 
         setData(productsWithCount);
-      })
-      .catch((err) => {
+      } catch (err) {
         console.error("Failed to fetch products:", err);
-      })
-      .finally(() => setIsLoading(false));
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProducts();
   }, []);
-  console.log(data);
+
   return (
     <ProductContext.Provider value={{ data, setData, isLoading }}>
       <NavigationBar />
       <Outlet />
       <Footer />
-      <Analytics />
       <ToastContainer position="bottom-center" />
     </ProductContext.Provider>
   );
